57756f56dbfa0ba3337595fa8ec20c23606ea646
keenanmalloy <malloyrkeenan@gmail.com>
2022-09-14 09:00:30 -0700

    slider

--- app/components/foodlog/FoodSearchModal.jsx
+++ app/components/foodlog/FoodSearchModal.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useCallback, useState, useEffect, useRef } from "react";
 import { AiOutlineClose, AiOutlineFire, AiOutlineSearch } from "react-icons/ai";
 import { GiKnifeFork } from "react-icons/gi";
 import {
@@ -11,12 +11,11 @@ import { BsDot } from "react-icons/bs";
 import { BiBarcodeReader, BiBook } from "react-icons/bi";
 import { FaRobot } from "react-icons/fa";
 import { RiFridgeLine } from "react-icons/ri";
-import { useSwipeable } from "react-swipeable";
-import { useEffect } from "react";
 
 export const FoodSearchModal = ({ setIsModalVisible }) => {
   const [isVisible, setIsVisible] = useState(true);
-  const [y, setY] = useState(0);
+  const [y, setY] = useState(50);
+  const [height, setHeight] = useState(0);
 
   const options = {
     weekday: "long",
@@ -25,7 +24,7 @@ export const FoodSearchModal = ({ setIsModalVisible }) => {
   const hours = ((today.getHours() + 11) % 12) + 1;
 
   return (
-    <div className="bg-slate-50 h-[915px] max-h-full overflow-y-hidden">
+    <div className="bg-slate-50 h-[915px] max-h-full overflow-hidden">
       {isVisible ? (
         <div className="flex justify-between items-center p-2">
           <button onClick={() => setIsModalVisible(false)}>
@@ -64,7 +63,7 @@ export const FoodSearchModal = ({ setIsModalVisible }) => {
           <SliderDot />
         </div>
       ) : null}
-      <div className="flex justify-center space-x-5 text-slate-300 fixed right-0 left-0 top-40 -z-50">
+      <div className="flex justify-center space-x-5 text-slate-300 fixed right-0 left-0 top-40 ">
         <GiKnifeFork />
 
         <p>Add one or more items below</p>
@@ -73,6 +72,8 @@ export const FoodSearchModal = ({ setIsModalVisible }) => {
         <LogSlider
           y={y}
           setY={setY}
+          height={height}
+          setHeight={setHeight}
           isVisible={isVisible}
           setIsVisible={setIsVisible}
         />
@@ -81,28 +82,63 @@ export const FoodSearchModal = ({ setIsModalVisible }) => {
   );
 };
 
-const LogSlider = ({ y, setY, setIsVisible }) => {
-  useEffect(() => {
-    if (y > 725) {
+const LogSlider = ({ y, setY, setIsVisible, height, setHeight }) => {
+  const [touchStart, setTouchStart] = useState(null);
+  const [touchEnd, setTouchEnd] = useState(null);
+
+  // the required distance between touchStart and touchEnd to be detected as a swipe
+  const minSwipeDistance = 0;
+
+  const onTouchStart = (e) => {
+    setTouchEnd(null); // otherwise the swipe is fired even with usual touch events
+    setTouchStart(e.targetTouches[0].clientY);
+  };
+
+  const onTouchEnd = () => {
+    if (!touchStart || !touchEnd) return;
+    const distance = touchStart - touchEnd;
+    const isDownSwipe = distance > minSwipeDistance;
+    const isUpSwipe = distance < -minSwipeDistance;
+    if (isDownSwipe || isUpSwipe)
+      console.log("swipe", isUpSwipe ? "down" : "up");
+    // add your conditional logic here
+    if (isUpSwipe) {
+      console.log("idiot");
+    }
+    if (isDownSwipe) {
       setY(725);
-    } else if (y > 400) {
+    }
+  };
+  useEffect(() => {
+    if (y > 400) {
       setIsVisible(true);
     } else if (y < 50) {
-      setY(50);
       setIsVisible(false);
     }
   }, [y]);
 
-  console.log(y);
+  const handleSwiping = (e) => {
+    const desiredYBottom = 725;
+    const desiredYTop = 49;
+    const swipeY = e.touches[0].clientY - 118;
+    // setTouchEnd();
+
+    setY(
+      swipeY < desiredYTop
+        ? desiredYTop
+        : desiredYBottom < swipeY
+        ? desiredYBottom
+        : swipeY
+    );
+  };
 
   return (
     <>
       <div
-        className="bg-white "
-        onTouchMove={(e) => setY(e.touches[0].clientY)}
-        // onTouchEnd={() => }
+        className="bg-white"
         style={{
           transform: `translateY(${y}px)`,
+          height: `${y - height}px`,
         }}
       >
         <div className="space-x-3 pt-2 absolute right-0 -top-12 ">
@@ -114,8 +150,21 @@ const LogSlider = ({ y, setY, setIsVisible }) => {
           </button>
         </div>
         <div>
-          <div className="flex justify-center items-center bg-white pt-2">
-            <SliderBar />
+          <div className="flex justify-center">
+            <button
+              onTouchMove={handleSwiping}
+              onTouchStart={onTouchStart}
+              onTouchEnd={onTouchEnd}
+              // Use touch start and touch end events to compare the
+              // y position to see if you are moving up or down.
+
+              // Maybe store the y position in a state variable for comparison.
+              // onTouchStart={}
+              // onTouchEnd={}
+              className="flex justify-center items-center bg-white pt-2 px-60"
+            >
+              <SliderBar />
+            </button>
           </div>
 
           <div className="pt-2 ">
@@ -123,7 +172,7 @@ const LogSlider = ({ y, setY, setIsVisible }) => {
           </div>
         </div>
       </div>
-      <div className="bg-white h-[900px]"></div>
+      <div className="bg-white h-[900px] "></div>
     </>
   );
 };
